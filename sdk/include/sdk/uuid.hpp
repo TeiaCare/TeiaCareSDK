4809da7208d06ff8311990bc88814cea15b99d78
Stefano Lusardi <stefano.lusardi@teiacare.com>
2023-08-08 18:01:39 +0200

    Added uuid docs.

--- sdk/include/sdk/uuid.hpp
+++ sdk/include/sdk/uuid.hpp
@@ -6,17 +6,51 @@ namespace tc::sdk
 {
 class uuid_generator;
 
+/*! \class uuid
+ *  \brief UUID V4.
+ *
+ * UUID object can be created empty using the default tc::sdk::uuid constructor or
+ * must be created using tc::sdk::uuid_generator::create method.
+ */
 class uuid
 {
     friend class tc::sdk::uuid_generator;
 
 public:
+    /*!
+     * \brief Constructor.
+     *
+     * Create an empty instance of a tc::sdk::uuid object.
+     */
     explicit uuid() = default;
+
+    /*!
+     * \brief Copy constructor.
+     *
+     * Copy a tc::sdk::uuid instance to another one.
+     */
     uuid(const uuid& other);
+
+    /*!
+     * \brief Assignment operator.
+     *
+     * Assign a tc::sdk::uuid instance to another one.
+     */
     uuid& operator=(const uuid& other);
-    
-    std::string bytes() const;    
+
+    /*!
+     * \brief Get the uuid bytes representation.
+     */
+    std::string bytes() const;
+
+    /*!
+     * \brief Get the uuid string representation.
+     */
     std::string str() const;
+
+    /*!
+     * \brief Get the uuid hash representation.
+     */
     size_t hash() const;
 
     friend bool operator==(const uuid& lhs, const uuid& rhs);
--- sdk/include/sdk/uuid_generator.hpp
+++ sdk/include/sdk/uuid_generator.hpp
@@ -5,12 +5,38 @@
 
 namespace tc::sdk
 {
+/*! \class uuid_generator
+ *  \brief UUID Generator used to create tc::sdk::uuid objects.
+ *
+ *  This class is a singleton that must be used to create instances of tc::sdk::uuid objects.
+ */
 class uuid_generator
 {
 public:
+    /*!
+     * \brief Static method to retrieve the single instance to the underlying generator.
+     *
+     * Note that the constructor is private, thus it is not possible to create multiple uuid_generator instances.
+     */
     static uuid_generator& instance();
+
+    /*!
+     * \brief Create a random uuid V4.
+     */
     uuid create();
+    
+    /*!
+     * \brief Create a tc::sdk::uuid object from a std::string.
+     *
+     * In case the provided string is not a valid uuid V4 a new valid uuid is returned.
+     */
     uuid from_string(const std::string& s);
+    
+    /*!
+     * \brief Create a tc::sdk::uuid object from a const char*.
+     *
+     * In case the provided string is not a valid uuid V4 a new valid uuid is returned.
+     */
     uuid from_string(const char* raw);
 
 private:
