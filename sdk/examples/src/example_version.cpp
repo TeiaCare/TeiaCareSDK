17562eb476dbc4d6d12381c394d09d34c0fa509a
Stefano Lusardi <stefano.lusardi@teiacare.com>
2023-08-22 15:59:15 +0200

    version reviewed. Added version example.

---
+++ sdk/examples/src/example_version.cpp
@@ -0,0 +1,22 @@
+#include <sdk/version.hpp>
+#include <iostream>
+
+int main() 
+{
+    std::cout << tc::sdk::info::name << "\n";
+    std::cout << "Version: " << tc::sdk::info::version << "\n";
+
+    std::cout << "\n" << tc::sdk::info::project_description << "\n";
+    std::cout << tc::sdk::info::project_url << "\n";
+    
+    std::cout << "\nBuild Type: " << tc::sdk::info::build_type << "\n";
+    std::cout << "Compiler: " << tc::sdk::info::compiler_name << " " << tc::sdk::info::compiler_version << "\n";
+    std::cout << "Compiler Flags: " << tc::sdk::info::cxx_flags << " " << tc::sdk::info::cxx_flags_debug << "\n";
+    std::cout << "C++ Standard: " << tc::sdk::info::cxx_standard << "\n";
+
+    std::cout << "\nOS: " << tc::sdk::info::os_name << "\n";
+    std::cout << "OS Version: " << tc::sdk::info::os_version << "\n";
+    std::cout << "Processor Architecture: " << tc::sdk::info::os_processor << "\n";
+
+    return 0;
+}
---
+++ sdk/src/version.cpp
@@ -0,0 +1,22 @@
+namespace tc::sdk::info
+{
+extern const char* const name = "TeiaCoreSDK";
+extern const char* const version = "1.0.0";
+
+extern const char* const project_description = "TeiaCoreSDK is a collection of reusable C++ components";
+extern const char* const project_url = "https://dev.azure.com/teiacare/Ancelia/_git/TeiaCoreSDK";
+
+extern const char* const build_type = "Debug";
+extern const char* const compiler_name = "GNU";
+extern const char* const compiler_version = "12.3.0";
+
+extern const char* const cxx_flags = " -march=native";
+extern const char* const cxx_flags_debug = "-g";
+extern const char* const cxx_flags_release = "-O3 -DNDEBUG";
+extern const char* const cxx_standard = "20";
+
+extern const char* const os_name = "Linux";
+extern const char* const os_version = "6.2.0-26-generic";
+extern const char* const os_processor = "x86_64";
+
+}
