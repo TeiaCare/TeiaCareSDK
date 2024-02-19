98bea736e07100b7095ea2505ec8fb910435d3d3
Roberto Delfiore <roberto.delfiore@teiacare.com>
2024-02-19 16:45:40 +0100

    Added copyright license header

--- sdk/include/sdk/task_scheduler.hpp
+++ sdk/include/sdk/task_scheduler.hpp
@@ -1,3 +1,17 @@
+// Copyright 2024 TeiaCare
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include <sdk/clock.hpp>
@@ -20,7 +34,7 @@ namespace tc::sdk
  * \brief Task Scheduler that can launch tasks on based several time-based policies
  *
  * This class is used to manage a queue of tasks using a fixed number of threads.  
- * The actual task execution is delgated to an internal tc::sdk::task_pool object.
+ * The actual task execution is delegated to an internal tc::sdk::task_pool object.
  */
 class task_scheduler : private non_copyable, private non_moveable
 {
