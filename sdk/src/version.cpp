7adbbe75c33bc0474b95e08a6843858e86c69d5f
Stefano Lusardi <stefano.lusardi@teiacare.com>
2024-02-19 14:41:52 +0100

    Cleanup version.cpp autogenerated fields. [skip ci]

--- sdk/src/version.cpp
+++ sdk/src/version.cpp
@@ -1,22 +1,22 @@
 namespace tc::sdk::info
 {
-extern const char* const name = "teiacore_sdk";
-extern const char* const version = "1.0.0";
+extern const char* const name = "";
+extern const char* const version = "";
 
-extern const char* const project_description = "TeiaCoreSDK is a collection of reusable C++ components";
-extern const char* const project_url = "https://dev.azure.com/teiacare/Ancelia/_git/TeiaCoreSDK";
+extern const char* const project_description = "";
+extern const char* const project_url = "";
 
-extern const char* const build_type = "Debug";
-extern const char* const compiler_name = "GNU";
-extern const char* const compiler_version = "12.3.0";
+extern const char* const build_type = "";
+extern const char* const compiler_name = "";
+extern const char* const compiler_version = "";
 
 extern const char* const cxx_flags = "";
-extern const char* const cxx_flags_debug = "-g";
-extern const char* const cxx_flags_release = "-O3 -DNDEBUG";
-extern const char* const cxx_standard = "20";
+extern const char* const cxx_flags_debug = "";
+extern const char* const cxx_flags_release = "";
+extern const char* const cxx_standard = "";
 
-extern const char* const os_name = "Linux";
-extern const char* const os_version = "6.5.0-17-generic";
-extern const char* const os_processor = "x86_64";
+extern const char* const os_name = "";
+extern const char* const os_version = "";
+extern const char* const os_processor = "";
 
 }
