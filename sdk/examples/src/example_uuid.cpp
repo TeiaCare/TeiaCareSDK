bbcabd6a387b87bd1d523e3c719fd2d540269832
Stefano Lusardi <lusardi.stefano@gmail.com>
2024-03-27 22:19:37 +0100

    Update example_uuid.cpp

--- sdk/examples/src/example_uuid.cpp
+++ sdk/examples/src/example_uuid.cpp
@@ -31,21 +31,8 @@ int main()
     {
         spdlog::info("\n\ntc::sdk::uuid::uuid() [null/empty UUID]");
         auto null = tc::sdk::uuid();
-        spdlog::info(null.str());
+        spdlog::info(null.to_string());
     }
-
-    {
-        spdlog::info("\n\ntc::sdk::uuid::bytes()");
-        auto uuid = uuid_generator.create();
-        spdlog::info(uuid.bytes());
-    }
-
-    {
-        spdlog::info("\n\ntc::sdk::uuid::hash()");
-        auto uuid = uuid_generator.create();
-        spdlog::info(uuid.hash());
-    }
-
     spdlog::info("\n\ntc::sdk::uuid_generator::from_string()");
     {
         auto uuid = uuid_generator.from_string("a142011e-58fa-4415-a0ea-e3ace6597fc8");
@@ -54,14 +41,14 @@ int main()
 
     {
         auto uuid = uuid_generator.from_string("ef25b786-d431-4612-8fb9-42afd24d22c6");
-        spdlog::info(uuid.str());
+        spdlog::info(uuid.to_string());
     }
 
     spdlog::info("\n\ntc::sdk::uuid_generator::create()");
     for (int i = 0; i < 10; ++i)
     {
         auto uuid = uuid_generator.create();
-        spdlog::info(uuid.str());
+        spdlog::info(uuid.to_string());
     }
 
     return 0;
