3ca4c848b475818bd5f755edfd5bf155c3cc350d
Stefano Lusardi <stefano.lusardi@teiacare.com>
2024-03-27 23:47:12 +0100

    Update example_uuid.cpp

--- sdk/examples/src/example_uuid.cpp
+++ sdk/examples/src/example_uuid.cpp
@@ -28,21 +28,13 @@ int main()
 {
     auto uuid_generator = tc::sdk::uuid_generator::instance();
 
-    {
-        spdlog::info("\n\ntc::sdk::uuid::uuid() [null/empty UUID]");
-        auto null = tc::sdk::uuid();
-        spdlog::info(null.to_string());
-    }
+    spdlog::info("\n\ntc::sdk::uuid::uuid() [null/empty UUID]");
+    auto null = tc::sdk::uuid();
+    spdlog::info(null.to_string());
+    
     spdlog::info("\n\ntc::sdk::uuid_generator::from_string()");
-    {
-        auto uuid = uuid_generator.from_string("a142011e-58fa-4415-a0ea-e3ace6597fc8");
-        spdlog::info(uuid.str());
-    }
-
-    {
-        auto uuid = uuid_generator.from_string("ef25b786-d431-4612-8fb9-42afd24d22c6");
-        spdlog::info(uuid.to_string());
-    }
+    auto uuidstr = uuid_generator.from_string("a142011e-58fa-4415-a0ea-e3ace6597fc8");
+    spdlog::info(uuidstr.to_string());
 
     spdlog::info("\n\ntc::sdk::uuid_generator::create()");
     for (int i = 0; i < 10; ++i)
